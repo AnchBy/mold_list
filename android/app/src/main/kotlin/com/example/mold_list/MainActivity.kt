504cc29a26b0785eda1f5155a6803df2ab5d67fa
Andrei Chakhovich <cmak666@gmail.com>
2026-04-12 10:39:59 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/mold_list/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.mold_list
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/data/mold.dart
@@ -0,0 +1,11 @@
+class Mold {
+  final String inventoryNumber;
+  final String moldName;
+  final int moldWeight;
+
+  Mold({
+    required this.inventoryNumber,
+    required this.moldName,
+    required this.moldWeight,
+  });
+}
